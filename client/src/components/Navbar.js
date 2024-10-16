14542d15b4a991496c4e659573214085be605362
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2024-10-16 16:02:46 +0530

    tailwind configured

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,16 @@
 // client/src/components/Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; // Optional: for styling
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="logo">YourLogo</div>
-            <ul className="nav-links">
-                <li><Link to="#services">Our Services</Link></li>
-                <li><Link to="#partners">Our Partners</Link></li>
-                <li><Link to="/lender">Lender</Link></li>
-                <li><Link to="/borrower">Borrower</Link></li>
+        <nav className="bg-gray-800 p-4">
+            <div className="text-white text-lg font-bold">YourLogo</div>
+            <ul className="flex space-x-4">
+                <li><Link className="text-red-300 hover:text-white" to="#services">Our Services</Link></li>
+                <li><Link className="text-gray-300 hover:text-white" to="#partners">Our Partners</Link></li>
+                <li><Link className="text-gray-300 hover:text-white" to="/lender">Lender</Link></li>
+                <li><Link className="text-gray-300 hover:text-white" to="/borrower">Borrower</Link></li>
             </ul>
         </nav>
     );
