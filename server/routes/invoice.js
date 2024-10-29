3f7046c1d68451de9842db2137714a0691dd2f66
sarman03 <sarmanpreets.it.22@nitj.ac.in>
2024-10-29 18:10:27 +0530

    cloudinary

--- server/routes/invoice.js
+++ server/routes/invoice.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const Invoice = require('../models/invoice');
+const cloudinary = require('../utils/cloudinaryConfig');
 
 router.post('/', async (req, res) => {
     try {
         console.log('Received data:', req.body); // Log the received data
+
+        // Upload file to Cloudinary in the 'pfi' folder
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            folder: 'pfi' // Specify the folder name here
+        });
+
         const newInvoice = new Invoice({
-            fileUpload: req.file.path, // Save the file path
+            fileUpload: result.secure_url, // Save the Cloudinary URL
             typeOfBusiness: req.body.typeOfBusiness,
             tenureOfInvoice: req.body.tenureOfInvoice,
             interestRate: req.body.interestRate
         });
+
         await newInvoice.save();
-        res.status(201).json({ message: 'Invoice Submitted' });
+        res.status(201).json({ message: 'Invoice Submitted', fileUpload: result.secure_url });
     } catch (error) {
         console.error('Error creating invoice:', error); // Log the error
         res.status(400).json({ message: 'Error creating invoice', error });
