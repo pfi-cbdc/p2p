af83a71252e1fd7840b998d889cb32807bf7962b
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2024-10-31 16:24:17 +0530

    Revert "Merge branch 'main' of https://github.com/pfi-cbdc/invoice_mng"
    This reverts commit c7a09b0da31f17b5bd1d7710e8520276fecc383d, reversing
    changes made to b8a7989dc29631409fff8766cdb167160bec7a7b.
    
    Reverting the commit

--- server/routes/borrower.js
+++ server/routes/borrower.js
@@ -14,20 +14,5 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Route to check borrower status
-router.get('/status', async (req, res) => {
-    const { email } = req.query;
-    try {
-        const borrower = await Borrower.findOne({ email });
-        if (borrower) {
-            return res.status(200).json({ exists: true });
-        }
-        return res.status(200).json({ exists: false });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Error checking borrower status', error: error.message });
-    }
-});
-
 module.exports = router;
 
