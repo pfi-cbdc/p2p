48ff387e5bcc52b1928e3991c668cbea39887fbc
chandershekhar22 <chandersj.it.22@nitj.ac.in>
2024-11-09 19:08:32 +0530

    home page content

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,14 @@
 // client/src/components/Home.js
 import React from 'react';
 import FAQ from './FAQ';
+import Point_image from './Point_image';
+import Hero from './Hero';
 const Home = () => {
     return (
         <div>
+            <Hero/>
            <FAQ/>
+           <Point_image/>
         </div>
     );
 };
