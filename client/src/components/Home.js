a076cb6b9c9d7ae2b1abd7b36b8f2da5a5fea3a7
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2024-11-09 22:03:23 +0530

    warnings satisfied

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,14 +1,15 @@
 // client/src/components/Home.js
 import React from 'react';
 import FAQ from './FAQ';
-import Point_image from './Point_image';
+import PointImage from './Point_image';
 import Hero from './Hero';
+
 const Home = () => {
     return (
         <div>
             <Hero/>
-           <FAQ/>
-           <Point_image/>
+            <FAQ/>
+            <PointImage/>
         </div>
     );
 };
