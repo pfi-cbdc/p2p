9d1068573f6bff9116ed642efcb17a3b51d8170d
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2024-11-09 23:33:10 +0530

    reverting the last commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,15 +3,12 @@ import React from 'react';
 import FAQ from './FAQ';
 import PointImage from './Point_image';
 import Hero from './Hero';
-
-import Footer from './Footer';
 const Home = () => {
     return (
         <div>
             <Hero/>
            <FAQ/>
-           <Point_image/>
-           <Footer/>
+           <PointImage/>
         </div>
     );
 };
