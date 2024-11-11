42ed7bb79f5f2f7deaaa448a5f52145928fa2536
va24nsh <vanshmaurya17@gmail.com>
2024-11-11 23:15:21 +0530

    Payment Integrationv1

--- client/src/components/OpenInvoices.js
+++ client/src/components/OpenInvoices.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import api from '../api/axios';
 const OpenInvoices = () => {
     const [response, setResponse] = useState(null);
+    const [amount, setAmount] = useState(1);
     const fetchInvoices = async () => {
         try {
             const res = await api.get('/api/invoices/all');
@@ -32,6 +33,8 @@ const OpenInvoices = () => {
                             <th className="border px-4 py-2">Tenure of Invoice</th>
                             <th className="border px-4 py-2">Interest Rate</th>
                             <th className="border px-4 py-2">Status</th>
+                            <th className="border px-4 py-2">Custom Amount</th>
+                            <th className="border px-4 py-2">Pay Now!</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -49,6 +52,50 @@ const OpenInvoices = () => {
                                     <td className="border px-4 py-2">{invoice.tenureOfInvoice}</td>
                                     <td className="border px-4 py-2">{invoice.interestRate}</td>
                                     <td className="border px-4 py-2">Accepted</td>
+                                    <td className="border px-4 py-2">
+                                    <input
+                                        type="number"
+                                        placeholder="Enter amount"
+                                        onChange={(e) => setAmount(e.target.value)}
+                                        className="border px-2 py-1"
+                                    />
+                                    </td>
+                                    <td className="border px-4 py-2">
+                                        <button onClick={async (e) => {
+                                        e.preventDefault();
+                                        const { data: { key } } = await api.get("/api/getkey")
+                                        const { data: { order } } = await api.post("/api/razor/checkout", {
+                                            amount: Number(amount)
+                                        })
+                                        const options = {
+                                            key,
+                                            amount: order.amount,
+                                            currency: "INR",
+                                            name: "P-Fi",
+                                            description: "Payment to P-fi",
+                                            image: "/public/favicon.ico",
+                                            order_id: order.id,
+                                            notes: {
+                                                address: "P-fi Office",
+                                                invoiceID: invoice._id,
+                                                lenderEmail: localStorage.getItem('email')
+                                            },
+                                            callback_url: "https://pfi-dwxi.onrender.com/api/razor/paymentverification",
+                                            prefill: {
+                                                name: "P-Fi",
+                                                email: "someone@somewhere.com",
+                                                contact: "********46"
+                                            },
+                                            theme: {
+                                                "color": "#121212"
+                                            }
+                                        };
+                                        const razor = new window.Razorpay(options);
+                                        razor.open();
+                                        }} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                            Pay
+                                        </button>
+                                    </td>
                                 </tr>
                             ))
                         }
---
+++ server/controllers/payments.js
@@ -0,0 +1,76 @@
+const crypto = require("crypto");
+const Payment = require("../models/Payment.js");
+const Borrower = require("../models/Borrower.js");
+const invoice = require("../models/invoice.js");
+const Razorpay = require('razorpay');
+const Lender = require("../models/Lender.js");
+const User = require("../models/User.js");
+require('dotenv').config();
+
+// RazorPay Instance
+const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_API_KEY,
+    key_secret: process.env.RAZORPAY_APT_SECRET,
+});
+
+const checkout = async (req, res) => {
+  
+  const options = {
+    amount: Number(req.body.amount * 100),
+    currency: "INR",
+  };
+  const order = await instance.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+};
+
+const paymentVerification = async (req, res) => {
+    try {
+        const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+        const payment = await instance.payments.fetch(razorpay_payment_id);
+        const notes = payment.notes;
+        
+        const invoices = await invoice.findById(notes.invoiceID);
+        const borrower = await Borrower.findById(invoices.borrowerID);
+        const user = await User.findOne({email: notes.lenderEmail});
+        const lender = await Lender.findOne({userID: user._id});
+        
+        const body = razorpay_order_id + "|" + razorpay_payment_id;
+    
+        const expectedSignature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
+        .update(body.toString())
+        .digest("hex");
+    
+        const isAuthentic = expectedSignature === razorpay_signature;
+    
+      if (isAuthentic) {
+        const newPayment = new Payment({
+          razorpay_order_id,
+          razorpay_payment_id,
+          razorpay_signature,
+          borrowerID: borrower ? borrower._id : '',
+          lenderID: lender ? lender._id : ''
+        });
+        await newPayment.save();
+    
+        res.redirect(
+          `http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`   
+        );
+      } else {
+        res.status(400).json({
+          success: false,
+        });
+      }
+    } catch(err) {
+        return res.redirect(`http://localhost:3000/lender-dashboard`);
+    }
+};
+
+module.exports = {
+    checkout,
+    paymentVerification,
+};
